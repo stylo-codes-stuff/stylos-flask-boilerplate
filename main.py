8c6833aa378d01efaed0a4216e480a664286d90e
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-09 16:50:22 +0000

    made boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
 
+@app.route('/',methods=['GET','POST'])
+def homepage():
+    return render_template('base.html')
+if __name__ == '__main__':
+    app.run(debug=True,port='8080')
